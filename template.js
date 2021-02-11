f5f092568e1a0c01a06bea6d57e5db31bd97aa86
tobinbradley <tobin.bradley@gmail.com>
2021-02-11 14:25:46 -0500

    make the default template better

--- template.js
+++ template.js
@@ -4,6 +4,7 @@ const source = process.argv[2]
 const destination = process.argv[3]
 const count = process.argv[4]
 const filename = process.argv[5]
+const rndString = Math.random().toString(36).substring(5)
 
 const template = `
 do
@@ -17,24 +18,30 @@ DECLARE
 BEGIN
 
   -- create server connection
-  CREATE SERVER IF NOT EXISTS ogr_fdw_db
+  CREATE SERVER IF NOT EXISTS fdw_${rndString}
 	FOREIGN DATA WRAPPER ogr_fdw
 	OPTIONS (datasource 'Pg:dbname= user= host= password=', format 'PostgreSQL' );
 
 
   -- create foreign table
-  CREATE FOREIGN TABLE IF NOT EXISTS ogr_fdw_source
+  CREATE FOREIGN fdt_${rndString}
 	(
 
-	) SERVER "ogr_fdw_db" OPTIONS (layer '${source}');
+	) SERVER "fdw_${rndString}" OPTIONS (layer '${source}');
 
 
-  -- set test criteria
+  -- test row count
   SELECT
     count(*) into test_count
   FROM
-    ogr_fdw_source;
+    fdt_${rndString};
 
+  IF test_count >= test_mincount THEN
+    raise exception 'error: minimum row count failure';
+  END IF;
+
+
+  -- test table change
   SELECT COALESCE(obj_description('${destination}'::regclass), '') into test_md5_previous;
 
   SELECT
@@ -42,40 +49,38 @@ BEGIN
   FROM
     (
       SELECT *
-      FROM ogr_fdw_source
+      FROM fdt_${rndString}
       ORDER BY 1
     ) AS t;
 
-  -- run update if tests passed
-  IF test_count >= test_mincount THEN
-	IF test_md5_current <> test_md5_previous THEN
-	  TRUNCATE TABLE ${destination} RESTART IDENTITY;
-
-      INSERT INTO ${destination}
-        (
-
-        )
-        SELECT
-
-        FROM
-          ogr_fdw_source;
-
-	  EXECUTE 'COMMENT ON TABLE ${destination} IS ' || quote_literal(test_md5_current);
-	ELSE
-	  raise exception 'no change found';
-	END IF;
-  ELSE
-	raise exception 'error: minimum row count failure';
+  IF test_md5_current <> test_md5_previous THEN
+    raise exception 'no change found';
   END IF;
 
+
+  -- run update
+	TRUNCATE TABLE ${destination} RESTART IDENTITY;
+
+  INSERT INTO ${destination}
+    (
+
+    )
+  SELECT
+
+  FROM
+    fdt_${rndString};
+
+  EXECUTE 'COMMENT ON TABLE ${destination} IS ' || quote_literal(test_md5_current);
+
+
   -- remove server and foreign table
-  DROP SERVER IF EXISTS ogr_fdw_db CASCADE;
+  DROP SERVER IF EXISTS fdw_${rndString} CASCADE;
 
 END;
 $$ LANGUAGE plpgsql;
 `
 
-fs.writeFile(path.join("jobs", filename), template, function(err) {
+fs.writeFile(path.join("jobs", filename), template, { flag: 'wx' }, function(err) {
   if(err) {
     return console.log(err)
   }
